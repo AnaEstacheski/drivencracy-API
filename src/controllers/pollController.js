7542ca54c5e4c76d3743bbd4637942836767dc69
AnaEstacheski <flaviaestacheski@gmail.com>
2022-12-05 22:21:30 -0300

    feat: result votes

--- src/controllers/pollController.js
+++ src/controllers/pollController.js
@@ -30,20 +30,53 @@ export async function pollGetChoice(req, res) {
     const { id } = req.params
 
     try {
-        const research = await pollCollection
+        const pollExist = await pollCollection
             .findOne({ _id: new ObjectId(id) })
 
-        if (!research) {
+        if (!pollExist) {
             res.status(404).send("Poll not find");
             return
         }
         const response = await choiceCollection.find({ pollId: id })
-        .toArray()
+            .toArray()
         res.send(response)
 
     } catch (err) {
         console.log(err);
         res.status(500).send(err.message);
         return
+    }
+}
+
+export async function pollResult(req, res) {
+    const { id } = req.params
+
+    try {
+
+        const poll = await pollCollection.findOne({ _id: ObjectId(id) })
+        const choice = await choiceCollection.find({ pollId: ObjectId(id) }).toArray()
+
+        let imostVotes = 0
+        let mostVotes = 0
+        const votes = await Promise.all(choice.map(async el => {
+            const elvotes = await choiceCollection.find({ choiceId: el._id }).toArray()
+            return {
+                title: el.title,
+                votes: elvotes.length
+            }
+        }));
+
+        votes.forEach((el, i) => {
+            if (el.votes > mostVotes) {
+                mostVotes = el.votes
+                imostVotes = i
+            }
+        });
+
+        res.send({ ...poll, result: votes[imostVotes] })
+
+    } catch (error) {
+        res.sendStatus(500);
+        console.log(error)
     }
 }
--- src/routes/pollRoutes.js
+++ src/routes/pollRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { pollPost, pollGet, pollGetChoice } from '../controllers/pollController.js';
+import { pollPost, pollGet, pollGetChoice, pollResult } from '../controllers/pollController.js';
 import { pollValidation } from '../middlewares/pollValidation.js';
 
 const router = Router();
 router.post("/poll", pollValidation, pollPost);
 router.get("/poll", pollGet);
 router.get("/poll/:id/choice", pollGetChoice);
+router.get("/poll/:id/result", pollResult);
 
 export default router;
